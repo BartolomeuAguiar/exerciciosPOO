6f635406c31fd0f31761f15a922a568e87fe34cb
barto <barto@10.20.27.174>
2018-05-07 21:04:37 -0400

    apostila fj-11 exercicio 14.5.1 e 14.5.2

--- src/TestaArray.java
+++ src/TestaArray.java
@@ -4,10 +4,21 @@ public class TestaArray {
 		
 		Conta[] contas = new Conta[10];
 		
-		for (int i = 0; i < contas.length; i++) {
+		for (int i = 0; i < contas.length-1; i++) {
 			Conta conta = new Conta();
 			conta.deposita(i * 100);
+			contas[i]=conta;
 		}
+		
+		float total = 0;
+		
+		for(int i = 0; i<contas.length-1;i++) {
+			total+=contas[i].getSaldo();
+		}
+		
+		System.out.println("total de dinheiro no banco R$ "+total);
+		
+		System.out.println("A média de saldo é R$ "+total/contas.length);
 
 	}
 }
